9c1a0b1f1dbab93689e804a82be34bbe7114f166
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-02 17:51:07 -0500

    image resizing function reorganized

--- src/preparation.py
+++ src/preparation.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 import tensorflow as tf
+from PIL import Image
+from numpy import asarray
+import numpy as np
 
 
 def empty_train_valid_split_directory(dir_path):
@@ -38,30 +41,30 @@ def empty_folder_duplicate(target_dir, dest_dir):
         os.mkdir(Path(dest_dir) / i)
 
 
-def resize_images(target_dir, dest_dir,target_size=255):
+def resize_images(target_dir, dest_dir,target_size=255,image_format='.png'):
     """
     Take images from target_dir and paste resizes of it in dest_dir
     :param target_dir: Path for target directory containing original images
     :param dest_dir: Path for destination directory where resized images will go
     :param target_size: size of the largest dimension (height or width) while maintaining
     the smaller dimension consistent with the original aspect ratio
+    :param image_format: format of the original images
     :return:
     """
-
-    for j in os.listdir(class_path):
-        if 'png' in j:
-            image = Image.open(class_path / Path(j))
-            data = asarray(image)
-            [ro, col, ch] = data.shape
+    for img_name in os.listdir(target_dir):
+        if 'png' in img_name:
+            image = Image.open(target_dir / Path(img_name))
+            img_array = asarray(image)
+            [ro, col, ch] = img_array.shape
 
             if ro > col:
-                new_ro = 255
-                new_col = int(np.ceil(col / ro * 255))
+                new_ro = target_size
+                new_col = int(np.ceil(col / ro * target_size))
             else:
-                new_col = 255
+                new_col = target_size
                 new_ro = int(np.ceil(ro / col * new_col))
             data_reshaped = tf.image.resize(
-                data, size=[new_ro, new_col], method='bilinear')
+                img_array, size=[new_ro, new_col], method='bilinear')
             data = data_reshaped.numpy().astype('uint8')
             im = Image.fromarray(data)
-            im.save(class_path_dest / Path(j))
+            im.save(dest_dir / Path(img_name))

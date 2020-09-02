43bddb3e2faab2bfcd7ebbb5ebf05be0d3868735
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-02 17:23:02 -0500

    test and function for creating train and validation split directory written

--- src/preparation.py
+++ src/preparation.py
@@ -1,10 +1,27 @@
 import os
 from pathlib import Path
 
-def train_valid_split_directory(dir_name):
-    pass
 
-def empty_folder_duplicate(target_dir,dest_dir):
+def train_valid_split_directory(dir_path):
+    """Split a folder containing classes and split an empty duplicate
+    with train and validation empty subfolder
+    :param dir_path:data directory path that needs to be splitted
+    :return:
+    """
+    new_dir_path = Path('split_' + dir_path.name)
+    if os.path.exists(new_dir_path):
+        raise KeyError('{} already exists!'.format(new_dir_path.name))
+    os.mkdir(new_dir_path)
+    new_train_path = new_dir_path / Path('train')
+    new_validation_path = new_dir_path / Path('validation')
+    os.mkdir(new_train_path)
+    os.mkdir(new_validation_path)
+    for sub_dir in os.listdir(dir_path):
+        os.mkdir(new_train_path / Path(sub_dir))
+        os.mkdir(new_validation_path / Path(sub_dir))
+
+
+def empty_folder_duplicate(target_dir, dest_dir):
     '''
     target_dir = directory which holds image categories
     dest_dir = directory which will hold simplified images in each category
@@ -14,28 +31,29 @@ def empty_folder_duplicate(target_dir,dest_dir):
     # we want to exclude any hidden folders
     class_names = [i for i in class_names if '.' not in i]
     for i in class_names:
-        os.mkdir(Path(dest_dir)/i)
+        os.mkdir(Path(dest_dir) / i)
 
-def resize_images(target_dir,dest_dir):
+
+def resize_images(target_dir, dest_dir):
     target_path = Path(target_dir)
     dest_path = Path(dest_dir)
     for i in class_names:
-        class_path = os.path.join(dir_path/Path(i))
-        class_path_dest = os.path.join(dest_dir/Path(i))
+        class_path = os.path.join(dir_path / Path(i))
+        class_path_dest = os.path.join(dest_dir / Path(i))
         for j in os.listdir(class_path):
             if 'png' in j:
-                image = Image.open(class_path/Path(j))
+                image = Image.open(class_path / Path(j))
                 data = asarray(image)
-                [ro,col,ch] = data.shape
+                [ro, col, ch] = data.shape
 
-                if ro>col:
+                if ro > col:
                     new_ro = 255
-                    new_col = int(np.ceil(col/ro*255))
+                    new_col = int(np.ceil(col / ro * 255))
                 else:
-                    new_col=255
-                    new_ro = int(np.ceil(ro/col*new_col))
+                    new_col = 255
+                    new_ro = int(np.ceil(ro / col * new_col))
                 data_reshaped = tf.image.resize(
-                data,size=[new_ro,new_col],method='bilinear')
+                    data, size=[new_ro, new_col], method='bilinear')
                 data = data_reshaped.numpy().astype('uint8')
                 im = Image.fromarray(data)
-                im.save(class_path_dest/Path(j))
+                im.save(class_path_dest / Path(j))
--- tests/test_preparation.py
+++ tests/test_preparation.py
@@ -1,24 +1,29 @@
 import unittest
+from unittest.mock import patch
 from src.preparation import *
 import shutil
 
+
 class TestPreparation(unittest.TestCase):
-
-
     def setUp(self) -> None:
         self.data_dir = Path('temp')
+        self.new_data_dir = Path('split_temp')
         if os.path.exists(self.data_dir):
-            shutil.rm(self.data_dir)
-        #create a temporary directory to host class folders
+            shutil.rmtree(self.data_dir)
+        if os.path.exists(self.new_data_dir):
+            shutil.rmtree(self.new_data_dir)
+        # create a temporary directory to host class folders
         os.mkdir(self.data_dir)
-        #we have 10 classes in this project
+        # we have 10 classes in this project
         for i in range(10):
-            os.mkdir(Path(self.data_dir/Path(str(i+1))))
+            os.mkdir(Path(self.data_dir / Path(str(i + 1))))
 
     def test_train_valid_split_directory(self):
-        pass
+        train_valid_split_directory(self.data_dir)
+        split_path = Path('split_' + self.data_dir.name)
+        self.assertEqual(len(os.listdir(self.data_dir)), len(os.listdir(split_path/Path('train'))))
+        self.assertEqual(len(os.listdir(self.data_dir)), len(os.listdir(split_path / Path('validation'))))
 
     def tearDown(self) -> None:
         shutil.rmtree(self.data_dir)
-
-
+        shutil.rmtree(self.new_data_dir)

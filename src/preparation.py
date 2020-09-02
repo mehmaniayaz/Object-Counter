029a3a59c319bb466f8218ffca350f215e7c7b62
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-09-02 17:45:25 -0500

    function reorganization

--- src/preparation.py
+++ src/preparation.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import tensorflow as tf
 
 
 def empty_train_valid_split_directory(dir_path):
@@ -37,26 +38,30 @@ def empty_folder_duplicate(target_dir, dest_dir):
         os.mkdir(Path(dest_dir) / i)
 
 
-def resize_images(target_dir, dest_dir):
-    target_path = Path(target_dir)
-    dest_path = Path(dest_dir)
-    for i in class_names:
-        class_path = os.path.join(dir_path / Path(i))
-        class_path_dest = os.path.join(dest_dir / Path(i))
-        for j in os.listdir(class_path):
-            if 'png' in j:
-                image = Image.open(class_path / Path(j))
-                data = asarray(image)
-                [ro, col, ch] = data.shape
+def resize_images(target_dir, dest_dir,target_size=255):
+    """
+    Take images from target_dir and paste resizes of it in dest_dir
+    :param target_dir: Path for target directory containing original images
+    :param dest_dir: Path for destination directory where resized images will go
+    :param target_size: size of the largest dimension (height or width) while maintaining
+    the smaller dimension consistent with the original aspect ratio
+    :return:
+    """
 
-                if ro > col:
-                    new_ro = 255
-                    new_col = int(np.ceil(col / ro * 255))
-                else:
-                    new_col = 255
-                    new_ro = int(np.ceil(ro / col * new_col))
-                data_reshaped = tf.image.resize(
-                    data, size=[new_ro, new_col], method='bilinear')
-                data = data_reshaped.numpy().astype('uint8')
-                im = Image.fromarray(data)
-                im.save(class_path_dest / Path(j))
+    for j in os.listdir(class_path):
+        if 'png' in j:
+            image = Image.open(class_path / Path(j))
+            data = asarray(image)
+            [ro, col, ch] = data.shape
+
+            if ro > col:
+                new_ro = 255
+                new_col = int(np.ceil(col / ro * 255))
+            else:
+                new_col = 255
+                new_ro = int(np.ceil(ro / col * new_col))
+            data_reshaped = tf.image.resize(
+                data, size=[new_ro, new_col], method='bilinear')
+            data = data_reshaped.numpy().astype('uint8')
+            im = Image.fromarray(data)
+            im.save(class_path_dest / Path(j))
